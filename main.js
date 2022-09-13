ed0e61688899a63760a965aaf007882078f219e0
airsCode <112642024+airsCode@users.noreply.github.com>
2022-09-13 09:04:45 -0600

    Finished
    added all variables and finished project.
    Everything works as intended.

--- main.js
+++ main.js
@@ -14,6 +14,16 @@ let numCod = 0;
 let numSalmon = 0;
 let numTropical = 0;
 let numPuffer = 0;
+let codSteve = 0.7;
+let salSteve = 0.9;
+let tropSteve = 0.95;
+let codAlex = 0.1;
+let salAlex = 0.2;
+let tropAlex = 0.5;
+let codVill = 0.25;
+let salVill = 0.5;
+let tropVill = 0.75;
+
 
 // Add Event Listener to Calculate Button
 fishBtn.addEventListener("click", fishBtnClicked);
@@ -26,23 +36,28 @@ function fishBtnClicked() {
   // Catch fish based on character
   if (character === "steve") {
     // STEVE: Cod 70%, Salmon 20%, Tropical 5%, Puffer 5%
-    catchFish();
+    catchFish(codSteve, salSteve, tropSteve);
   } else if (character === "alex") {
     // ALEX: Cod 10%, Salmon 10%, Tropical 30%, Puffer 50%
-    catchFish();
+    catchFish(codAlex, salAlex, tropAlex);
+  } else if (character === "villager") { 
+    // VILLAGER: Cod 25%, Salmon 25%, Tropical 25%, Puffer 25%
+    catchFish(codVill, salVill, tropVill);
   }
+  
+}
 
-  function catchFish() {
+  function catchFish(fishOne, fishTwo, fishThree) {
     let randNum = Math.random();
-    if (randNum < 0.7) {
+    if (randNum < fishOne) {
       numCod++;
       numCodSpan.innerHTML = numCod;
       resultImg.src = "img/Raw-Cod.png";
-    } else if (randNum < 0.9) {
+    } else if (randNum < fishTwo) {
       numSalmon++;
       numSalmonSpan.innerHTML = numSalmon;
       resultImg.src = "img/Raw-Salmon.png";
-    } else if (randNum < 0.95) {
+    } else if (randNum < fishThree) {
       numTropical++;
       numTropicalSpan.innerHTML = numTropical;
       resultImg.src = "img/Tropical-Fish.png";
@@ -52,4 +67,4 @@ function fishBtnClicked() {
       resultImg.src = "img/Pufferfish.png";
     }
   }
-}
+
